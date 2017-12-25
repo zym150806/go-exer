60ef39e7e4ade1dee18c39bbe1c158bdb03b16c1
zhangyimeng <2218497554@qq.com>
2017-12-25 11:32:30 +0800

    default-selection

--- default-selection1.go
+++ default-selection1.go
@@ -1,1 +1,25 @@
+//当 select 中的其他分支都没准备好时，default分支就会运行
 package main
+
+import (
+	"time"
+	"fmt"
+)
+
+func main(){
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.Tick(500 * time.Millisecond)
+
+	for {
+		select {
+			case <-tick:
+				fmt.Println("tick.")
+			case <-boom:
+				fmt.Println("boom.")
+				return
+			default:
+				fmt.Println("    .")
+				time.Sleep(50 * time.Millisecond)
+		}
+	}
+}
